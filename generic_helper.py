754c7f00adbee7ea7d6ed4475de84982c5c3775e
Amey Narwadkar <69268308+ameynarwadkar@users.noreply.github.com>
2024-02-13 21:02:35 +0530

    Add files via upload

---
+++ generic_helper.py
@@ -0,0 +1,19 @@
+import re
+
+def extract_session_id(session_str: str):
+    match = re.search(r"sessions/(.*?)/contexts/", session_str)
+    if match:
+        extracted_string = match.group(1)
+        return extracted_string
+    
+    return ""
+
+def get_str_from_food_dict(food_dict: dict):
+    return ",".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+
+
+
+
+       # print(extract_session_id("projects/raiden-chatbot-co9h/agent/sessions/72d2e497-8e67-531b-31c6-38f56dbf1350/contexts/ongoing-order"))
+    
+    
